5ac8da72d8fc26a9f7c4e9eb071cfa106a79b7f7
Max Koverdyaev <shadeglare@gmail.com>
2016-09-24 12:59:09 +0300

    fixed examples

--- examples/Index.ts
+++ examples/Index.ts
@@ -3,8 +3,8 @@ import { h1, div, a, input } from "./../src/Html";
 window.onload = () => {
     console.log("welcome to expanse");
 
-    let title = div().className("title").text("Value:");
-    let value = div().className("value").text("0");
+    let title = div().class("title").text("Value:");
+    let value = div().class("value");
     let click =  e => {
         e.preventDefault();
         alert("click");
@@ -12,8 +12,8 @@ window.onload = () => {
     let link = a().attr("href", "#").text("link").on("click", click);
     let data = input().on("input", e => {
         console.log(e.target["value"]);
-    });
-    let block = div([title, value], link, data).className("block").appendTo(document.body);
+    }).val(0);
+    let block = div([title, value], link, data).class("block").appendTo(document.body);
 
     let x = 0;
 
@@ -25,7 +25,7 @@ window.onload = () => {
 
         if (x === 10) {
             value.detach();
-            title.removeClassName();
+            title.removeClass();
             link.off("click", click);
         }
     }, 200);
